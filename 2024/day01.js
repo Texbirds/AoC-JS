efa410b621712e720f293128958ba232fe866b88
Tex <kwintjanleenders@gmail.com>
2025-04-14 21:20:43 +0200

    Day01 part 1 done

--- 2024/day01.js
+++ 2024/day01.js
@@ -1,1 +1,20 @@
- 
+ function part1(input) {
+    // parse input, put in seperate lists
+    const lines = input.trim().split('\n');
+    const leftList = lines.map(line => +line.split(/\s+/)[0]).sort((a, b) => a - b);
+    const rightList = lines.map(line => +line.split(/\s+/)[1]).sort((a, b) => a - b);
+    
+    let totalDifference = 0; // let instead of const; value will change in for-loop
+
+    for (let i = 0; i < leftList.length; i++) {
+        totalDifference += Math.abs(leftList[i] - rightList[i])
+    }
+
+    return totalDifference;
+ }
+
+ function part2(input) {
+    return null;
+ }
+
+ module.exports = { part1, part2 };
