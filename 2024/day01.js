2a97e1d39bbb2326c385d5e7194941c1fb6d00fa
Tex <kwintjanleenders@gmail.com>
2025-04-14 21:47:29 +0200

    Day01 part 2 done

--- 2024/day01.js
+++ 2024/day01.js
@@ -14,7 +14,27 @@
  }
 
  function part2(input) {
-    return null;
+    const lines = input.trim().split('\n');
+    const leftList = lines.map(line => +line.split(/\s+/)[0]).sort((a, b) => a - b);
+    const rightList = lines.map(line => +line.split(/\s+/)[1]).sort((a, b) => a - b);
+
+    // create frequencymap. Avoids nested loops, in which case we would have an O(n²) time complexity
+    const frequencyMap = new Map()
+    for (let i = 0; i < rightList.length; i++) {
+        const value = rightList[i];
+        const count = frequencyMap.get(value) || 0;
+        frequencyMap.set(value, count + 1);
+    }
+
+    // calculate the final answer
+    let totalSimilarity = 0;
+    for (let i = 0; i < leftList.length; i++) {
+        const currentValue = leftList[i];
+        const currentValueCount = frequencyMap.get(currentValue) || 0;
+        totalSimilarity += (currentValue * currentValueCount)
+    }
+
+    return totalSimilarity;
  }
 
  module.exports = { part1, part2 };
