e584930b7760097fe07ed50e94350d8f65c0d21d
Tex <kwintjanleenders@gmail.com>
2025-04-16 10:24:44 +0200

    Day06 part 2 done

--- 2024/day06.js
+++ 2024/day06.js
@@ -56,7 +56,84 @@ function part1(input) {
 }
 
 function part2(input) {
-    return null;
+    const grid = input.trim().split('\n').map(line => line.split(''));
+    let visited = 0; // not a Set anymore but just an integer, need to track "already marked" locations too
+    let loopsFound = 0;
+
+    // map for directions
+    const directions = {
+        'up': [-1, 0],
+        'right': [0, 1],
+        'down': [1, 0],
+        'left': [0, -1]
+    }
+
+    // map for the next turn
+    const rightTurn = {
+        'up': 'right',
+        'right': 'down',
+        'down': 'left',
+        'left': 'up'
+    }
+
+    let startLocation = null;
+    let currentLocation = null;
+    let direction = 'up'; // always starts facing up
+
+    // find current location
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            const cell = grid[row][col];
+            if (cell === '^') {
+                startLocation = [row, col];
+                currentLocation = [row, col];
+                break;
+            }
+        }
+    }
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            // temporarily put obstacle at location
+            let cell = grid[row][col];
+            if (cell === '#' || cell === '^') {
+                continue;
+            } else {
+                grid[row][col] = '#'
+            }
+            // go through loop like normal
+            while(true) {
+                const [dRow, dCol] = directions[direction];
+                const nextRow = currentLocation[0] + dRow;
+                const nextCol = currentLocation[1] + dCol;
+        
+                if (
+                    nextRow < 0 || nextRow >= grid.length ||
+                    nextCol < 0 || nextCol >= grid[0].length
+                ) {
+                    break;
+                } else if (grid[nextRow][nextCol] === '#') {
+                    direction = rightTurn[direction];
+                } else {
+                    currentLocation = [nextRow, nextCol];
+                    visited++;
+                }
+
+                // if the guard has visited a lot more than he normally would, we can assume he's in a loop
+                if(visited > 6000) {
+                    loopsFound++;
+                    break;
+                }
+            }
+            // reset variables to starting positions and whatnot
+            currentLocation = startLocation;
+            direction = 'up'
+            visited = 0;
+            grid[row][col] = '.'
+        }
+    }
+      
+    return loopsFound;
 }
 
 module.exports = { part1, part2 };
